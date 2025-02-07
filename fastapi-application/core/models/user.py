0e902a36688ab88b9ae0851d9d5dce1bb96dac9a
Flamesone2 <sergbasov.com@gmail.com>
2025-02-07 22:54:24 +0300

    Cleaning unused imports

--- fastapi-application/core/models/user.py
+++ fastapi-application/core/models/user.py
@@ -1,11 +1,5 @@
-from collections.abc import AsyncGenerator
 from typing import TYPE_CHECKING
-
-from fastapi import Depends
 from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
-from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
-from sqlalchemy.orm import DeclarativeBase
-
 from core.models import Base
 from core.models.mixins.int_id_pk import IntIdPkMixin
 from core.types.user_id import UserId_type
